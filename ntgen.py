5e41bb7936a057a86be67e56d8607853b9e64c1b
yuricaprini <yuricaprini@gmail.com>
2022-06-16 18:37:14 +0200

    added filters for IP

--- ntgen.py
+++ ntgen.py
@@ -1,27 +1,35 @@
 from email.policy import HTTP
 import sys
 import logging
+import ipaddress
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 from scapy.all import *
 
-if len(sys.argv)!=4:
-    print('Usage: sudo python ntgen.py [nPackets] [onInterface] [dstMAC]')
-    print('-dstMAC is the destination MAC address. It is formatted as six sets of two digits or characters, separated by colons:')
-    print(' e.g. 03:00:72:3a:14:32')
-    sys.exit(-1)
+if len(sys.argv)!=5:
+	print('Usage: sudo python ntgen.py [nPackets] [onInterface] [dstMAC] [pathToPcap]')
+	print('-dstMAC is the destination MAC address. It is formatted as six sets of two digits or characters, separated by colons:')
+	print(' e.g. 03:00:72:3a:14:32')
+	sys.exit(-1)
 
 nPackets = int(sys.argv[1])
 onIface = str(sys.argv[2])
 dstMAC = str(sys.argv[3])
+pathToPcap = str(sys.argv[4])
 
 #TODO: loopback case must be fixed
 #ip = get_if_addr(onIface)
 for x in range(nPackets):
-    #if ip=='127.0.0.1':
-        #sendp(Loopback()/IP(src=RandIP(), dst=RandIP())/TCP, iface=onIface)
-    pkt=Ether(dst=dstMAC)/IP(src=RandIP()._fix(), dst=RandIP()._fix())/UDP()
-    print(pkt[IP].src,'-->',pkt[IP].dst)
-    sendp(pkt, iface=onIface, verbose=0)
+	src_ip = ipaddress.ip_address(str(RandIP()._fix()))
+	while src_ip.is_multicast or src_ip.is_private or src_ip.is_loopback or src_ip.is_link_local:
+		src_ip = ipaddress.ip_address(str(RandIP()._fix()))
 
+	
+	dst_ip = ipaddress.ip_address(str(RandIP()._fix()))
+	while dst_ip.is_multicast or dst_ip.is_private or dst_ip.is_loopback or dst_ip.is_link_local:
+		dst_ip = ipaddress.ip_address(str(RandIP()._fix()))
+	
+	pkt=Ether(dst=dstMAC)/IP(src = format(src_ip), dst = format(dst_ip))/UDP()
+	print(pkt[IP].src,'-->',pkt[IP].dst)
+	wrpcap(pathToPcap, pkt, append=True)
 
